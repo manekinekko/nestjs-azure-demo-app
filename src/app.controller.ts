7b33a7c21b09880807d3c15f56edeaca57f03c5c
Wassim Chegham <github@wassim.dev>
2019-08-09 15:16:18 +0200

    wip

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  UseInterceptors,
+  UploadedFile,
+  Logger,
+} from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Post('upload')
+  UploadedFilesUsingInterceptor(
+    @UploadedFile()
+    file: any,
+  ) {
+    
   }
 }
